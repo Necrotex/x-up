da5e6b619655bec294e3b0bf7672dda723b8e78b
Necrotex <necrotex@gmail.com>
2015-04-25 21:42:58 +0200

    more progress

---
+++ x-up/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace x_up
+{
+    public partial class Form1 : Form
+    {
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HTCAPTION = 0x2;
+        [DllImport("User32.dll")]
+        public static extern bool ReleaseCapture();
+        [DllImport("User32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        public Form1()
+        {
+            InitializeComponent();
+ 
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+            }
+        }
+
+        
+    }
+}
---
+++ x-up/Logs.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+
+namespace x_up
+{
+    class Logs
+    {
+        public static int xCoutner = 0;
+        public static int lastLine = 0;
+
+        private static DirectoryInfo dirInfo;
+
+        private static List<FileInfo> fleetLogList;
+        private static FileInfo fleetLog;
+        private static bool fileLock = false;
+
+
+        public static void getLatestFleetLog()
+        {
+            dirInfo = new DirectoryInfo(Configuration.logDir);
+
+            fleetLogList = new List<FileInfo>(dirInfo.GetFiles("Fleet_*"));
+            fleetLogList.OrderBy(x => x.CreationTime).ToList<FileInfo>();
+            fleetLog = fleetLogList.Last<FileInfo>();
+
+            readLog();
+
+            Form1.label2.text = xCoutner;
+
+        }
+
+        public static void readLog()
+        {
+      
+            using(StreamReader log = new StreamReader(Path.Combine(Configuration.logDir, fleetLog.Name)))
+            {
+                string logLine;
+                int lineNum = 0;
+
+                while ((logLine = log.ReadLine()) != null)
+                {
+                    lineNum++;
+
+                    // Magic number 13: The first 13 lines of a log are only meta data.
+                    if (lineNum <= 13) continue;
+
+                    if (lineNum > lastLine)
+                    {
+                        logLine = logLine.Remove(0, logLine.IndexOf(">") + 2);
+
+                        if (logLine == Configuration.searchString)
+                        {
+                            xCoutner++;
+                        }
+
+                        lastLine = lineNum;
+                    }                    
+                }
+
+                
+            }
+
+
+        }
+
+    }
+}
